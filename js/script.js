c11d5d094fc2c849a90cced06f43e597abff0301
Sesughter01 <charles.avul@gmail.com>
2022-11-06 08:09:35 +0100

    javascript branch added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// This is a single line javascript comment
+/*
+  This is a multi-line javascript comment
+*/
+
+/*
+  Javascript is an interpreted scripting or programming language: This means
+  that an interpreter converts it directly into machine readable code and executes it
+
+  Javascript is used to make webpages dynamic; it can be used manipulate the html document and css styles
+
+*/
+
